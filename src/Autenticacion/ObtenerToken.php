8f1c48786e304ab6ad7c22c55d9ccc8f1fd0dd83
Globalbtek <tecnologia1@globalbtek.com>
2022-03-13 05:27:32 -0600

    avance de la librería de la API de fiscuscfdi.com de PHP

--- src/Autenticacion/ObtenerToken.php
+++ src/Autenticacion/ObtenerToken.php
@@ -2,10 +2,17 @@
 declare(strict_types=1);
 namespace FiscusCFDI\ApiFiscusCFDIPHP\Autenticacion;
 use FiscusCFDI\ApiFiscusCFDIPHP\General\UrlGeneral; 
+use FiscusCFDI\ApiFiscusCFDIPHP\General\Peticion; 
 /**
  * 
  */
 class ObtenerToken
 { 
-
+    public static function getToken($usuario, ·$password)
+    {
+        $url=UrlGeneral::getUrl()."api_obtener_token";
+        $parametros=array("usuario"=>$usuario, "password"=>$password);
+        $respuesta=Peticion::peticion($url,$parametros,$post=true,$credencial=array());
+        return $respuesta; 
+	}
 }
